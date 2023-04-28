4d64011f29028d0722bc1b68ea0cd6980183f96c
Rafael Gonzaga <ralozaga@gmail.com>
2023-04-28 15:22:11 -0300

    Commit final - Projeto acompanhado

---
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImp.java
@@ -0,0 +1,52 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.MatriculaRepository;
+import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatriculaServiceImp implements IMatriculaService {
+
+    @Autowired
+    private MatriculaRepository repository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public Matricula create(MatriculaForm form) {
+        Matricula matricula = new Matricula();
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        matricula.setAluno(aluno);
+
+        return repository.save(matricula);
+    }
+
+    @Override
+    public Matricula get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Matricula> getAll(String bairro) {
+        if(bairro == null)
+            return repository.findAll();
+//        return repository.findByAlunoBairro(bairro);
+
+        // Usando a querie
+        return repository.findAlunosMatriculadosBairro(bairro);
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
